4404e260009972bcba590d0db1cf3436872ab3b1
Kabir Gupta <85513702+KabirGupta07@users.noreply.github.com>
2023-01-19 17:44:28 +0530

    BASIC SETUP

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+const axios = require("axios");
 var xhub = require('express-x-hub');
 
 const PORT  = process.env.PORT || 6000;
@@ -36,40 +37,17 @@ app.get(['/facebook','/instagram','/whatsapp'], function(req, res) {
   }
 });
 
-app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
-
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.sendStatus(401);
-    return;
-  }
-
-  console.log('request header X-Hub-Signature validated');
-  // Process the Facebook updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
 app.post('/whatsapp', function(req, res) {
   console.log('whatsapp request body:');
   console.log(req.body);
   // Process the Instagram updates here
   received_updates.unshift(req.body);
 
-  res.sendStatus(200);
-
   axios.post("http://3.110.149.13:4600/conversation/chat", {
     data: req.body
   });
+  return res.sendStatus(200);
+
 });
 
 app.listen(PORT, ()=>{
